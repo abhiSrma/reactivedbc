6216543396c5170b821e2c60a7d33979bd148f0f
Abhishek Sharma <37658401+abhiSrma@users.noreply.github.com>
2023-06-25 16:33:17 +0530

    Init

---
+++ src/main/java/com/reactive/dbc/reactivedbc/ReactivedbcApplication.java
@@ -0,0 +1,13 @@
+package com.reactive.dbc.reactivedbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactivedbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactivedbcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reactive/dbc/reactivedbc/ReactivedbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactive.dbc.reactivedbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactivedbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
